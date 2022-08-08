dbca55c6f02ebaadf009ae6c33df2737f80965ee
Paolo <paolocordovacontacto@gmail.com>
2022-08-08 18:59:54 -0400

    Basic structure done

---
+++ src/components/Common/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../assets/styles/components/Footer.scss';
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+const base_url ='https://api.themoviedb.org/3/search/movie?';
+const api_key ='23409b71e4666ec61b3283faca372336';
+//const api_access_token='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzQwOWI3MWU0NjY2ZWM2MWIzMjgzZmFjYTM3MjMzNiIsInN1YiI6IjYyZjE3ZDkzY2VlMmY2MDA3YThkZGI5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y6VQB9kOAceadtXcbZibovwL3J8BFkSB5AUS27x_0Gg';
+
+export const movieGet = movie => `${base_url}api_key=${api_key}&query=${movie}&page=1&include_adult=false`;
